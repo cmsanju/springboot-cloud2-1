43a257424f0635ce73aa0842468b634eb173fd76
cmsanju <lict.sanju@gmail.com>
2023-08-28 17:52:05 +0530

    first commit

---
+++ src/main/java/com/test/SpringbootCloud21Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCloud21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloud21Application.class, args);
+	}
+
+}
